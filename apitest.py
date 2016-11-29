8bfe8336f7386c53a5f5be5d664ce9598e87a8a1
K-42 <K-42@users.noreply.github.com>
2016-11-29 12:16:00 +0000

    - Updated API credentials to read from separate local file. 	- Added local file to .gitignore. 	- Add example credential file.

--- apitest.py
+++ apitest.py
@@ -1,7 +1,17 @@
 import requests, json
 
-auth = {"apikey":"","username":"","userkey":""}
-data = json.dumps(auth)
-headers = {"Content-Type": "application/json"}
-getToken = requests.post('https://api.thetvdb.com/login', data=data, headers=headers).json()
-token = {"Authorization: Bearer " + (getToken['token'])}
+def login():
+    try:
+        with open('local.auth') as f:
+            auth = f.read()
+        headers = {"Content-Type": "application/json"}
+        getToken = requests.post('https://api.thetvdb.com/login', data=auth, headers=headers).json()
+        token = {"Authorization: Bearer " + (getToken['token'])}
+        print("Login successful.")
+    except FileNotFoundError:
+        print ("Login failed. API credentials for TVDB have not been set.")
+
+#adding .json() to the end of getToken turns it into a dictionary, which makes it easier to separate the token part of the response. decodes?
+#without this, the response item is a string: I couldn't get json.load/s to work on this for some reason.
+
+
