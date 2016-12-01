903498976f9f04c27db0b39f307a6328895a9c07
K-42 <K-42@users.noreply.github.com>
2016-12-01 13:47:20 +0000

    Update and rename apitest.py to login.py

--- login.py
+++ login.py
@@ -10,8 +10,3 @@ def login():
         print("Login successful.")
     except FileNotFoundError:
         print ("Login failed. API credentials for TVDB have not been set.")
-
-#adding .json() to the end of getToken turns it into a dictionary, which makes it easier to separate the token part of the response. decodes?
-#without this, the response item is a string: I couldn't get json.load/s to work on this for some reason.
-
-
