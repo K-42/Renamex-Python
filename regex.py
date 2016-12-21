6183b7f3b07f58eeedba04faff81b5d624e1e3a0
K-42 <k_miller42@hotmail.com>
2016-12-21 10:31:31 +0000

    Update + created class for filename functions + renamed "sSeason" and "sEpisode" to "sNum" and "eNum" + forgot to put file extension back into filename so created "fileExt" + put all regex variables inside function namer(): + simplified filename loop

--- regex.py
+++ regex.py
@@ -4,65 +4,75 @@ files = ['Dexter- S01E10.1080p.5.1Ch.BluRay.ReEnc-DeeJayAhmed.mp4','Breaking Bad
 
 #regex
 pull= re.compile(r'''
-	([a-z-_\.\s]*) 						#series name
-	(s(\d{,2})e(\d{,2})|(\d{3,4})|(\d{1,2})x(\d{1,2}))      #episode number (S01E01, 1x1, 101)
-	(.*) 							#episode title (junk)
-	(\.\w*$) 						#file extension
-        ''',re.VERBOSE|re.I)
-
+	([a-z-_\.\s]*) 										#series name
+	(s(\d{,2})e(\d{,2})|(\d{3,4})|(\d{1,2})x(\d{1,2})) 	#episode number (S01E01, 1x1, 101)
+	(.*) 												#episode title (junk)
+	(\.\w*$) 											#file extension
+    ''',re.VERBOSE|re.I)
+	
 caps = "y"
-opt = 1
+choice = "opt1"
 eTitle = "Temporary"
 
-def opt1(): #S01E01
-	filename = ((sName) 
-	+ " - " 
-	+ ("S" if caps == "y" else "s") 
-	+ (str(sSeason) if int(sSeason) >=10 else "0"+str(sSeason)) 
-	+ ("E" if caps == "y" else "e") 
-	+ (str(sEpisode) if int(sEpisode) >=10 else "0"+str(sEpisode)) 
-	+ " - " 
-	+ eTitle)
-	return filename
-  
-def opt2(): #S1E1
-	filename = ((sName)
-	+ " - "
-	+ ("S" if caps == "y" else "s")
-	+ str(sSeason)
-	+ ("E" if caps == "y" else "e")
-	+ str(sEpisode)
-	+ " - "
-	+ eTitle)
-	return filename
-  
-def opt3(): #1x1
-	filename = ((sName)
-	+ " - " 
-	+ str(sSeason)
-	+ ("X" if caps == "y" else "x") 
-	+ str(sEpisode) 
-	+ " - " 
-	+ eTitle)
-	return filename
-  	
-def opt4(): #101
-	filename = ((sName) 
-	+ " - " 
-	+ str(sSeason) 
-	+ (str(sEpisode) if int(sEpisode) >=10 else "0"+str(sEpisode))
-	+ " - "
-	+ eTitle)
-	return filename
+def namer(x):
+  oldName = pull.search(files[x])
+  sName = re.sub(r'[\W_]',' ', oldName.group(1)).title().strip(' ') #removes junk and trailing white space from series name
+  sNum = oldName.group(2).strip('s,S')[:2].lstrip('0').strip('x,X,e,E')
+  eNum = oldName.group(2)[-2:].lstrip('0').strip('x,X,e,E')
+  fileExt = oldName.group(9)
+  return sName, sNum, eNum, fileExt
 
-#main processing loop
-j=0
+class opt():
+  @staticmethod
+  def opt1(x): #S01E01
+        sName = namer(x)[0]
+        sNum = namer(x)[1]
+        eNum = namer(x)[2]
+        fileExt = namer(x)[3]
+        filename = ((sName) 
+        + " - " 
+        + ("S" if caps == "y" else "s") 
+        + (str(sNum) if int(sNum) >=10 else "0"+str(sNum)) 
+        + ("E" if caps == "y" else "e") 
+        + (str(eNum) if int(eNum) >=10 else "0"+str(eNum)) 
+        + " - "
+        + eTitle
+        + fileExt) 
+        return filename
+  @staticmethod
+  def opt2(x): #S1E1
+        filename = ((sName)
+  	+ " - "
+  	+ ("S" if caps == "y" else "s")
+  	+ str(sSeason)
+  	+ ("E" if caps == "y" else "e")
+  	+ str(sEpisode)
+  	+ " - "
+  	+ eTitle)
+  	return filename
+  @staticmethod
+  def opt3(): #1x1
+  	filename = ((sName)
+  	+ " - " 
+  	+ str(sSeason)
+  	+ ("X" if caps == "y" else "x") 
+  	+ str(sEpisode) 
+  	+ " - " 
+  	+ eTitle)
+  	return filename
+  @staticmethod
+  def opt4(): #101
+  	filename = ((sName) 
+  	+ " - " 
+  	+ str(sSeason) 
+  	+ (str(sEpisode) if int(sEpisode) >=10 else "0"+str(sEpisode))
+  	+ " - "
+  	+ eTitle)
+  	return filename
+
+#print files
+x=0
 for i in files:
-	oldName = pull.search(files[j])
-	sName = re.sub(r'[\W_]',' ', oldName.group(1)).title().strip(' ') #removes junk and trailing white space from series name
-	sSeason = oldName.group(2).strip('s,S')[:2].lstrip('0').strip('x,X,e,E')
-	sEpisode= oldName.group(2)[-2:].lstrip('0').strip('x,X,e,E')
-	if opt == 1:
-                file = opt1()
-		print (file)
-		j+=1
+  newFile = getattr(opt, choice)(x)
+  print (newFile)
+  x+=1
