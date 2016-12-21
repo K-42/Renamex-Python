3018f93eb407803c27729c7739dc4c74140ed021
K-42 <k_miller42@hotmail.com>
2016-12-21 09:47:30 +0000

    Update + improved formatting methods in filename loop + added multiple filename output versions

--- regex.py
+++ regex.py
@@ -1,20 +1,26 @@
 import re
 
-files = ['Dexter- s01E01.1080p.5.1Ch.BluRay.ReEnc-DeeJayAhmed.mp4','Breaking Bad 1x01 720p.BRrip.Sujaidr.mp4','The.Walking.Dead - 101 - Web of Lies - .1080p.WEB-DL.x264-FUM[ettv].txt']
+files = ['Dexter- S01E10.1080p.5.1Ch.BluRay.ReEnc-DeeJayAhmed.mp4','Breaking Bad 1x13 720p.BRrip.Sujaidr.mp4','The.Walking.Dead - 1104 - Web of Lies - .1080p.WEB-DL.x264-FUM[ettv].txt', 'extant - s14e14 .mp4' ]
 
+#regex
 pull= re.compile(r'''
-	([a-z-_\.\s]*) 							#series name
-	(s\d{,2}e\d{,2}|\d{3,4}|\dx\d{1,2}) 	#episode number (S01E01, 101, 1X01)
-	(.*) 									#episode title (junk)
-	(\.\w*$) 								#file extension
-    ''',re.VERBOSE|re.I)
-	
+	([a-z-_\.\s]*) 						#series name
+	(s(\d{,2})e(\d{,2})|(\d{3,4})|(\d{1,2})x(\d{1,2}))	#episode number (S01E01, 1x1, 101)
+	(.*) 							#episode title (junk)
+	(\.\w*$) 						#file extension
+        ''',re.VERBOSE|re.I)
+
+#main processing loop
 j=0
 for i in files:
-  oldName = pull.search(files[j])
-  seriesTitle = re.sub(r'[\W_]',' ', oldName.group(1)).title()
-  #if episode title = yes, then get title before manage white space, else:
-  manageWhitespace = re.sub(r'\s+',' ', seriesTitle)
-  newName = manageWhitespace + "- " + oldName.group(2).upper() + oldName.group(4)
-  print(newName)
-  j+=1
+        oldName = pull.search(files[j])
+        sName = re.sub(r'[\W_]',' ', oldName.group(1)).title().strip(' ') #removes junk and trailing white space from series name
+        sSeason = oldName.group(2).strip('s,S')[:2].lstrip('0').strip('x,X,e,E')
+        sEpisode= oldName.group(2)[-2:].lstrip('0').strip('x,X,e,E')
+        #filenames
+        #print((sSeason) +','+(sEpisode))
+        #print((sName) +	" - S" + (sSeason if int(sSeason) >=10 else "0"+str(sSeason)) + "E" + (sEpisode if int(sEpisode) >=10 else "0"+str(sEpisode))) #if 0s +title
+        #print((sName) + " - S" + (sSeason) + "E" + (sEpisode)) #if not 0s +title
+        #print((sName) + " - " + (sSeason) + "x" + (sEpisode)) #if 1x1 +title
+        #print((sName) + " - " + (sSeason) + (sEpisode if int(sEpisode) >=10 else "0"+str(sEpisode))) #if 101 +title
+        j+=1
